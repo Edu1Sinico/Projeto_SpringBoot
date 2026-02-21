c11e99917c903e83632117bc54ccfe61336feebd
Edu1Sinico <EduardoSenai888@gmail.com>
2026-02-21 19:42:49 -0300

    Adicionando o primeiro projeto de revisão do Spring Boot para o Github - 21/02/2026

---
+++ projeto_spring_revisao/src/main/java/com/example/projeto_spring_revisao/ProjetoSpringRevisaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.projeto_spring_revisao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringRevisaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringRevisaoApplication.class, args);
+	}
+
+}
---
+++ projeto_spring_revisao/src/test/java/com/example/projeto_spring_revisao/ProjetoSpringRevisaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projeto_spring_revisao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringRevisaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
